81970c261cc4aee021ff1062eb261abec521ed21
shivanshu <shivanshumangal007@gmail.com>
2026-02-11 17:15:22 +0530

    student Dashboard Route ADDED

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from "./pages/Login.jsx"
+import StudentDashboard from "./pages/StudentDashboard.jsx"
+import ProtectedRoute from './components/ProtectedRoutes'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='bg-amber-500'>helllo from app </h1>
-    </div>
-  )
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Login />} path='/login' />
+					<Route
+            path='/'
+						element={
+							<ProtectedRoute>
+								<StudentDashboard />
+							</ProtectedRoute>
+						}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</>
+  );
 }
 
 export default App
