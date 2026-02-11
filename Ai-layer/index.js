6f245437b6fbbfa2747623c7c0efa9c156b289bd
shivanshu <shivanshumangal007@gmail.com>
2026-02-12 00:11:23 +0530

    some

--- Ai-layer/index.js
+++ Ai-layer/index.js
@@ -1,1 +1,18 @@
-console.log("hello from agent");
+import readline from "readline/promises";
+
+async function main() {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+	while (true) {
+		const input = await rl.question("Enter a command: ");
+		if (input.toLowerCase() === "exit") {
+			console.log("Exiting...");
+			break;
+		}
+		console.log(`You entered: ${input}`);
+	}
+    rl.close();
+}
+await main();
--- frontend/src/pages/StudentDashboard.jsx
+++ frontend/src/pages/StudentDashboard.jsx
@@ -142,6 +142,7 @@ const StudentDashboard = () => {
 	};
 
 	const getStatusColor = (status) => {
+		status = status.toLowerCase();
 		const statusColors = {
 			pending: "bg-yellow-100 text-yellow-800",
 			in_progress: "bg-blue-100 text-blue-800",
@@ -152,6 +153,7 @@ const StudentDashboard = () => {
 	};
 
 	const getPriorityColor = (priority) => {
+		priority = priority.toLowerCase();
 		const priorityColors = {
 			low: "bg-green-100 text-green-800",
 			medium: "bg-yellow-100 text-yellow-800",
