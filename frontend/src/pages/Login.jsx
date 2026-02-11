cc4792b881c8956e613899c364e2b47fe8d821b1
shivanshu <shivanshumangal007@gmail.com>
2026-02-11 22:47:48 +0530

    login and student dashboard updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import api from "../utils/api";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../utils/constants";
+import Logo from "../assets/images/logo.png";
+
 
 const Login = () => {
 	const [formData, setFormData] = useState({
@@ -55,20 +57,8 @@ const Login = () => {
 			<div className='w-full max-w-md'>
 				{/* Header */}
 				<div className='text-center mb-8'>
-					<div className='inline-flex items-center justify-center w-16 h-16 bg-green-100 rounded-full mb-4'>
-						<svg
-							className='w-8 h-8 text-green-600'
-							fill='none'
-							stroke='currentColor'
-							viewBox='0 0 24 24'
-						>
-							<path
-								strokeLinecap='round'
-								strokeLinejoin='round'
-								strokeWidth={2}
-								d='M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z'
-							/>
-						</svg>
+					<div className='inline-flex items-center justify-center w-20 h-20 bg-green-100 rounded-full mb-1'>
+						<img src={Logo} alt="ResolveIt Logo" className="w-30" />
 					</div>
 					<h1 className='text-3xl font-bold text-gray-800 mb-2'>
 						ResolveIt
