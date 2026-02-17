49ca66b84d2a840e1b3ee96297beb7d9d30ffef1
Hannu Verma <hannuverma78@gmail.com>
2026-02-17 09:59:22 +0530

    alert delete function

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -181,6 +181,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME', 'dwe6n6goq'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY', '786111112512728'),
